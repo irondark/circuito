d1dc1d142893804d9d99fbe8d60d1532a66d2786
irondark <naldo.duarte06@gmail.com>
2021-03-01 15:56:15 -0300

    mejora

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function () {
+  function reorient(e) {
+    var portrait = (window.orientation % 180 == 0);
+    $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+  }
+  window.onorientationchange = reorient;
+  window.setTimeout(reorient, 0);
+});
+
 const canvasContainer = document.querySelector('#canvas-container');
 const barraHerramientas = document.querySelector('#barra-herramientas');
 const botonesBarraHerramientas = document.querySelectorAll('#barra-herramientas button');
